f11a3ad55fc7107def44de16f27f65e6bbe3f6e6
Gavin Burnell <g.burnell@leeds.ac.uk>
2021-03-22 15:06:11 +0000

    Tweak exported symbols and attempt to make conda recipe pick up version from package.

--- phys2320_assessor/__init__.py
+++ phys2320_assessor/__init__.py
@@ -3,14 +3,25 @@
 Module of functions for doing Computing 2 Assessment
 """
 
-__all__=["cohort","result","funcs","assessor", "Assessor","ComputingClass"]
+__all__ = [
+    "cohort",
+    "result",
+    "funcs",
+    "assessor",
+    "Assessor",
+    "ComputingClass",
+    "Result",
+    "read_user_data",
+    "parse_code",
+]
 
-__version__="2020.0.1"
+__version__ = "2020.0.2"
 
 from .assessor import Assessor
 from .cohort import ComputingClass
-from.funcs import parse_code
+from .funcs import parse_code, read_user_data
+from .result import Result
 
-if __name__=="__main__":
-    filename="model_solution.py"
-    stmt=parse_code(filename)
+if __name__ == "__main__":
+    filename = "model_solution.py"
+    stmt = parse_code(filename)
