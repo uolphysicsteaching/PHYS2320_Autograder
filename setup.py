4c628e222ac32bdc5ef81e77815d987ae544480e
Gavin Burnell <g.burnell@leeds.ac.uk>
2021-03-22 15:35:09 +0000

    More integration of conda build and setup.py

--- setup.py
+++ setup.py
@@ -14,5 +14,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     version=version(),
+    name = "phys230_assessor",
+    author = "Gavin Burnell",
+    author_email = "g.burnell@leeds.ac.uk",
+    description = "A tool for assessing Leeds physics student python coursework",
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/uolphysicsteaching/PHYS2320_Autograder",
+    classifiers =
+    ["Programming Language :: Python :: 3",
+    "License :: OSI Approved :: BSD 3 clause License",
+    "Operating System :: OS Independent"],
     long_description=long_description,
 )
