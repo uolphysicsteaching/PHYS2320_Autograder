3859a0b4a3781c159c57ec7a9739a515d16244ec
Gavin Burnell <g.burnell@leeds.ac.uk>
2024-03-29 13:37:44 +0000

    Fix bad dependency

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 import setuptools
 from pathlib import Path
 
+
 def version():
-    p=Path(__file__).parent/"phys2320_assessor"/"__init__.py"
+    p = Path(__file__).parent / "phys2320_assessor" / "__init__.py"
     for line in p.read_text().split("\n"):
-        line=line.strip()
+        line = line.strip()
         if line.startswith("__version__"):
-            vers=line.split("=")[1].strip(' "')
+            vers = line.split("=")[1].strip(' "')
             return vers
 
 
@@ -15,15 +16,16 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     version=version(),
-    name = "phys230_assessor",
-    author = "Gavin Burnell",
-    author_email = "g.burnell@leeds.ac.uk",
-    description = "A tool for assessing Leeds physics student python coursework",
-    long_description_content_type = "text/markdown",
-    url = "https://github.com/uolphysicsteaching/PHYS2320_Autograder",
-    classifiers =
-    ["Programming Language :: Python :: 3",
-    "License :: OSI Approved :: BSD 3 clause License",
-    "Operating System :: OS Independent"],
+    name="phys230_assessor",
+    author="Gavin Burnell",
+    author_email="g.burnell@leeds.ac.uk",
+    description="A tool for assessing Leeds physics student python coursework",
+    long_description_content_type="text/markdown",
+    url="https://github.com/uolphysicsteaching/PHYS2320_Autograder",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD 3 clause License",
+        "Operating System :: OS Independent",
+    ],
     long_description=long_description,
 )
